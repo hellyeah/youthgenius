51cf4d39955d6de9c63833a761d728a2ad8d72b8
gailees <gailees@umich.edu>
2013-02-22 02:00:46 -0500

    It's working

--- js/vote.js
+++ js/vote.js
@@ -2,31 +2,25 @@ $(document).ready(function(){
 	
 	Parse.initialize("cJuWw2KIaEhK5DsR26nqB2icj74QlUzu5XC4QhbO", "s5ORdEm9JqYLkSzjCiqXlIoFcBuMFRiAttSDPRSj");
 	
-	$(".form-wrapper").submit(function() {
+	$(".form-wrapper").submit(function(e) {
 
 		//Grab data from entry field and split by commas
 		var str= ($(".form").val());
-		var n=str.split(",");
-		//maybe they do newline split rather than commas
-		alert("blah");
+
 		//initialize db storage names
-		var Launchrock = Parse.Object.extend("launchEmails");
-		
-		//loop through array of separated numbers emails them to db
-		var emails = new launchEmails();
-			
-		//send that shit to parse
-		emails.save(
-			{email: n}, 
-			{
- 				success: function(object) {
-				alert("yay! it worked");
-				}
-			}
-		)
+		var LaunchEmails = Parse.Object.extend("LaunchEmails");
+		var launchEmails = new LaunchEmails();
+		launchEmails.save({email: str}, {
+		  success: function(object) {
+		    alert("yay! it worked");
+		  }
+		});
 		
 		//alert("!");
-		$(".form").hide();	
+		$(".form").val("");
+		e.preventDefault();
+		$(this).hide(1000);
+		return false;	
 	})
 
 	$("input").keypress(function(event) {
